e11e75eefb50354216719709415f6968c046ce96
heesu_choi <chlgmltn101@naver.com>
2021-01-04 20:03:16 +0900

    Update signaling server

--- index.js
+++ index.js
@@ -22,28 +22,30 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('message', function (message, room) {
         log('Client said : ', message);
-        //socket.broadcast.to(room).emit('message', message);
-        socket.broadcast.emit('message', message);
+        // socket.broadcast.to(room).emit('message', message);
+        socket.to(room).emit('message', message);
+    });
+
+    socket.on('streaming', function (room) {
+        socket.to(room).emit('play');
     });
 
     socket.on('create or join', function (room) {
-        log('Received request to create or join room' + room);
+        log('Create or Join room' + room);
 
         const clientsInRoom = io.sockets.adapter.rooms[room];
         const numClients = numClientsInRoom(clientsInRoom, room);
-        
+
         if (numClients === 0) {
             log('Client ID ' + socket.id + 'created room' + room);
             socket.join(room);
             socket.emit('created', room, socket.id);
-        }
-        else if (numClients == 1) {
+        } else if (numClients === 1) {
             log('Client ID ' + socket.id + 'joined room' + room);
             socket.join(room);
             socket.emit('joined', room, socket.id);
             io.sockets.in(room).emit('ready');
-        }
-        else {
+        } else {
             socket.emit('full', room);
         }
     });
@@ -61,10 +63,9 @@ io.sockets.on('connection', function (socket) {
 
 function numClientsInRoom(clientsInRoom, room) {
     if (clientsInRoom === undefined) {
-        console.log('Make room : ', room);
+        console.log('Create room : ', room);
         return 0;
-    }
-    else {
+    } else {
         console.log('Join room : ', room);
         return clientsInRoom.length;
     }
